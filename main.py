c2926fa18ac97d5c08c56a2903df1c1a1c674b6f
Douglas <d.bianchezzi@gmail.com>
2021-01-30 10:04:11 -0300

    Novos arquivos, inclusive um conversor de json pra csv sem indice

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+import pandas as pd
 
 #lendo json
 
 content = open('dados.json').read()
+
+json_do_pandas = pd.read_json(r'dados.json')
 
 pessoas = json.loads(content)
 
@@ -10,6 +13,15 @@ pessoas = json.loads(content)
 
 for item in pessoas:
     print(item)
+    
+print("--------------------------------------------------")
+
+#print(json_do_pandas)
+
+print(json_do_pandas.info())
+
+#for item2 in json_do_pandas:
+#    print(f'Nome: {item2[nome]}')
 
 
 # usando uma estrutura similar ao que já conhecemos em migrcoes,
